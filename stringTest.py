3edf6398ff7bcb6c8de6c1f0fb86aeeabc177b56
Akhmadjon <72850225+mrakhmadjon@users.noreply.github.com>
2024-06-17 12:30:11 +0200

    Update stringTest.py

--- stringTest.py
+++ stringTest.py
@@ -1,19 +1,17 @@
 import unittest
-from strlng import strlng
+from stringLen import stringLen
 
-print(strlng("asaa"))
+#print(stringLen("asaa"))
 
 class TestInput(unittest.TestCase):
     def test_length(self):
-        result = strlng("something")
+        result = stringLen("something")
         self.assertEqual(result,len("somethign"))
 
     def test_length2(self):
-        result = strlng("laaa")
+        result = stringLen("laaa")
         self.assertEqual(result,1)
 
 
-if(__name__) == '_main_':
-    unittest.main
-
-
+if __name__ == "__main__":
+    unittest.main()
